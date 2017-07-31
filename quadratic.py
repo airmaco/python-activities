37e7398484be65087fc4550000f9feb7e3da2a96
airmaco <30274459+airmaco@users.noreply.github.com>
2017-07-31 18:53:04 -0400

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,20 +1,10 @@
-#quadratic
-
 a=int(input("What is a equal to?"))
 b=int(input("What is b equal to?"))
 c=int(input("What is c equal to?"))
 x=int(input("What is x equal to?"))
 
-result = (a * (x*2) +(b*x) + c)
+result = (a*x*x +b*x +x)
 
 print("The following Quadratic was entered", a,'x^2',b,'x+',c)
 print("The value of the Quadratic is",result)
 
-input(int("What is a equal to?", a))
-input(int("What is b equal to?", b))
-input(int("What is c equal to?", c))
-input(int("What is x equal to?", x))
-
-answer = (a * (x^2) + (b*x) + c)
-print("The following Quadratic was entered, a,’X^2’b,’X’,c")
-print("The value of the Quadratic is",result)
